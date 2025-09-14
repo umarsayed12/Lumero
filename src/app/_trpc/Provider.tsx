81e6070473dd7f68c0b5cd7110add7550331d787
Umar Khursheed <umarkhursheed979@gmail.com>
2025-09-15 04:17:20 +0530

    Implemeted AI Typing Feature

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -11,7 +11,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `${process.env.URL}/api/trpc`,
+          url: `/api/trpc`,
         }),
       ],
     })
